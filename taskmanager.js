2c18880502a7217afe8baa512081a3e545ccc814
Damiano Bonaccorsi <50593663+ozerodb@users.noreply.github.com>
2021-03-29 17:55:38 +0200

    fix: shared-icon now appears for non-private tasks

--- taskmanager.js
+++ taskmanager.js
@@ -29,10 +29,10 @@ for (let [i,task] of tasks.tasks.entries()){
     new_task_label.innerText = task.description;
     new_task_checkbox_div.appendChild(new_task_label)
     if (task.isUrgent){
-        new_task_label.classList.add ("important");
+        new_task_label.classList.add("important");
     }
-    if (task.isPrivate){
-        new_task_checkbox_div.insertAdjacentHTML("beforeend" ,'<svg class="bi bi-person-square private-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/></svg>');
+    if (!task.isPrivate){
+        new_task_checkbox_div.insertAdjacentHTML("beforeend" ,'<svg class="bi bi-people-fill shared-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/><path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/></svg>');
     }
     if (task.deadline != undefined){
         let new_task_deadline = document.createElement('small');
